766c5671b870d2e1dd3bd4bd67ae0b3ca333d019
Luxy3000 <cilusu15@gmail.com>
2024-02-18 18:25:00 +0100

    HeaderController reworked

--- src/Controller/HeaderController.php
+++ src/Controller/HeaderController.php
@@ -5,12 +5,16 @@ namespace App\Controller;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Component\Routing\Annotation\Route;
 use Doctrine\ORM\EntityManagerInterface;
 
 use App\Entity\Genre;
-use App\Entity\Stories;
+use App\Entity\User;
+use App\Entity\Story;
 
+#[IsGranted('ROLE_USER')]
 class HeaderController extends AbstractController
 {
     #[Route(path:'/header', name: 'genre_page')]
@@ -18,12 +22,18 @@ class HeaderController extends AbstractController
     public function  genresList(EntityManagerInterface $entityManager): Response
     {
         $genres = $entityManager->getRepository(Genre::class)->findAll();
-        return $this->render('header.html.twig', array('genres'=>$genres));
+       
+        return $this->render('header.html.twig', array('genres' => $genres));
+    }
+    public function pfp(EntityManagerInterface $entityManager): Response{
+        $userPfp = $entityManager->getRepository(User::class)->find('photo');
+        return $this->render('header.html.twig', array('userPfp' => $userPfp) );
     }
 
-    #[Route(path: '/genres/{genreName}', name: 'show_genre')]
-    public function showGenres(EntityManagerInterface $entityManagerInterface): Response
+    #[Route(path: '/genres/{genreId}', name: 'show_genre')]
+    public function showGenres(EntityManagerInterface $entityManager, $genreID)
     {
-        $stories = $entityManagerInterface->getRepository(Stories::class)->findAll();
+        $stories = $entityManager->find(Story::class, $genreID);
+        return $this->render('stories.html.twig', array('stories'=>$stories));
     }
 }
