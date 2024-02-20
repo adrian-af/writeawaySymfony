5b5fd22a3108bacdbb33d5f618d1e8081fdb8455
Luxy3000 <cilusu15@gmail.com>
2024-02-20 20:35:08 +0100

    Data added to pass to the header twig

--- src/Controller/StoriesController.php
+++ src/Controller/StoriesController.php
@@ -5,6 +5,7 @@
 namespace App\Controller;
 use App\Entity\Genre;
 use App\Entity\Story;
+use App\Entity\User;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -17,9 +18,21 @@ use Doctrine\ORM\EntityManagerInterface;
 class StoriesController extends AbstractController
 {
     #[Route(path:'/helloUser', name: 'helloUser')]
-    public function hello()
-    {
-        return $this->render('hello.html.twig');
+    public function hello(EntityManagerInterface $entityManager)
+    {   
+        //For the header
+        $user = $this->getUser();
+        $genresHeader = $entityManager->getRepository(Genre::class)->findAll();
+        $userPfp = $user?->getPhoto();
+        $base64Pfp = null;
+        if ($userPfp !== null) {
+            $base64Pfp = 'data:image/jpg;charset=utf8;base64,' . base64_encode(stream_get_contents($userPfp));
+        }
+        return $this->render('hello.html.twig', [
+            //For header
+            'genres' => $genresHeader,
+            'userPfp' => $base64Pfp
+        ]);
     }
 
     #[Route(path:'/otherProfile', name: 'otherProfile')]
